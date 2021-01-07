04da2d7a5cc442487e2e38aed3a72092ebe0ba11
nikita.mirolyubov <mirolyubov_n@yahoo.co.uk>
2021-01-07 10:59:48 +0500

    refactoring - itemlist becomes proper itemlist (instead of people list)

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -7,13 +7,18 @@ import './app.css';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page/people-page';
+import SwapiService from '../../services/swapi-service';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
 
 export default class App extends Component {
 
-state = {
-	showRandomPlanet: true,
-	hasError: false
-};
+	swapiService = new SwapiService();
+
+	state = {
+		showRandomPlanet: true,
+		hasError: false
+	};
 
 toggleRandomPlanet = () => {
 	this.setState((state) => {
@@ -49,6 +54,31 @@ render() {
 			</button>
 			<ErrorButton/>
 		<PeoplePage/>
+
+		<div className="row mb2">
+				<div className="col-md-6">
+					<ItemList 
+						onItemSelected={this.onPersonSelected}
+						getData	={this.swapiService.getAllPlanets}
+						/>
+				</div>
+				<div className="col-md-6">
+					<PersonDetails personId={this.state.selectedPerson} />
+				</div>
+		</div>
+
+		<div className="row mb2">
+				<div className="col-md-6">
+					<ItemList 
+						onItemSelected={this.onPersonSelected}
+						getData	={this.swapiService.getAllStarships}
+						/>
+				</div>
+				<div className="col-md-6">
+					<PersonDetails personId={this.state.selectedPerson} />
+				</div>
+		</div>
+
 		</div>
 	);
 	}
--- star-db/src/services/swapi-service.js
+++ star-db/src/services/swapi-service.js
@@ -2,7 +2,7 @@ export default class SwapiService {
 
 	_apiBase = 'https://swapi.dev/api';
 
-	async getResource(url) {
+	getResource = async(url) => {
 		const res = await fetch(`${this._apiBase}${url}`);
 
 		if (!res.ok) {
@@ -12,39 +12,39 @@ export default class SwapiService {
 		return await res.json();
 	}
 
-	async getAllPeople() {
+	getAllPeople = async() => {
 		const res = await this.getResource(`/people/`);
 		return res.results.map(this._transformPerson);
 	}
 
-	async getPerson(id) {
+	getPerson = async(id) => {
 		const person = await this.getResource(`/people/${id}/`);
 		return this._transformPerson(person);
 	}
 
-	async getAllPlanets() {
+	getAllPlanets = async() => {
 		const res = await this.getResource(`/planets/`);
 		return res.results.map(this._transformPlanet);
 	}
 
-	async getPlanet(id) {
+	getPlanet = async(id) => {
 		const planet = await this.getResource(`/planets/${id}/`);
 		return this._transformPlanet(planet)
 	}
 
-	async getAllStarships() {
+	getAllStarships = async() =>{
 		const res = await this.getResource(`/starships/`);
 		return res.results.map(this._transformStarship);
 	}
 
-	async getStarship(id) {
+	getStarship = async(id) => {
 		const sh = await this.getResource(`/starships/${id}/`);
 		return (this._transformStarship(sh))
 	}
 
-	_extractId (planet) {
+	_extractId = item => {
 		const regExp = /\/([0-9]*)\/$/;
-		const id = planet.url.match(regExp)[1];
+		const id = item.url.match(regExp)[1];
 		return id;
 	}
 	_transformPlanet = (planet) => {
